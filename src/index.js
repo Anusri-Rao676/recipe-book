f6e3f38f7298a7092cb7e8941e694806bbe3549a
Anusri <anusriraok@gmail.com>
2024-01-14 23:41:09 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -37,10 +37,38 @@ app.post("/signup", async (req, res)=>{
         res.send("user already exits. please enter another name");
     }
     else{
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+        data.password = hashedPassword;
     const userdata = await collection.insertMany(data);
     console.log(userdata);
     }
 });
+
+app.post("/login", async (req, res) => {
+    try {
+        // Assuming 'collection' is your MongoDB collection
+        const check = await collection.findOne({ name: req.body.username });
+
+        if (!check) {
+            res.send("Username not found");
+            return; // Add a return statement to exit the function early
+        }
+
+        const isPasswordMatch = await bcrypt.compare(req.body.password, check.password);
+
+        if (isPasswordMatch) {
+            // Corrected: Use res.render without a leading '/'
+            res.render("recipe.ejs", { username: req.body.username });
+        } else {
+            res.send("Wrong password");
+        }
+    } catch (error) {
+        // Corrected: Use res.send to send a response, not req.send
+        res.send("Error: " + error.message);
+    }
+});
+
 
 
 const port = 5000;
